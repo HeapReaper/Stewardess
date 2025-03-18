7846bfff5f20cc2ff8cd5ac2b5d1c01ae22fe9ed
AeroBytes <kelvin@aerobytes.nl>
2025-03-18 11:13:22 +0100

    Added message delete/update log

--- modules/ServerLogger/events.ts
+++ modules/ServerLogger/events.ts
@@ -1,10 +1,41 @@
-import {Client, TextChannel} from 'discord.js';
+import { Client, Events, EmbedBuilder, TextChannel } from 'discord.js';
+import { getEnv } from '@helpers/env.ts';
+import { Logging } from '@helpers/logging';
+import { ColorEnum } from '@enums/ColorEnum';
 
 export default class ServerLoggerEvents {
 	private client: Client;
+	private logChannel!: TextChannel;
 	
 	constructor(client: Client) {
 		this.client = client;
+		// @ts-ignore
+		this.logChannel = this.client.channels.cache.get(getEnv('LOG') !== undefined ? getEnv('LOG') : Logging.error(('LOG env not found'))) as TextChannel;
+		this.messageLog();
 	}
 	
+	messageLog(): void {
+		this.client.on(Events.MessageDelete, async (message): Promise<void> => {
+			const embed = new EmbedBuilder()
+				.setColor(ColorEnum.Red)
+				.setTitle('Bericht verwijderd!')
+				.setDescription(`Door: <@${message.author?.id}> | Kanaal: <#${message.channel.id}>.`)
+				.addFields({ name: 'Content', value: message.content ?? 'Niet gevonden...' });
+			
+			await this.logChannel.send({ embeds: [embed] });
+		});
+		
+		this.client.on(Events.MessageUpdate, async (oldMessage, newMessage): Promise<void> => {
+			const embed = new EmbedBuilder()
+				.setColor(ColorEnum.Orange)
+				.setTitle('Bericht aangepast!')
+				.setDescription(`Door: <@${newMessage.author?.id}> | Kanaal: <#${newMessage.channel.id}>.`)
+				.addFields(
+					{name: 'Voor', value: oldMessage.content ?? 'Niet gevonden'},
+					{name: 'Na', value: newMessage.content ?? 'Niet gevonden'}
+				);
+			
+			await this.logChannel.send({ embeds: [embed] });
+		});
+	}
 }
