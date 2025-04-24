634c04317e3abaf687940539b360d161eee052f2
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-24 22:43:20 +0200

    Refactor logging utility for enhanced flexibility and clarity.
    Updated log file paths to use dynamic environment-based configuration. Improved type safety by allowing `string|number` for log messages. Added detailed JSDoc comments for better code documentation and maintenance.

--- utils/logging.ts
+++ utils/logging.ts
@@ -1,22 +1,30 @@
-import { getEnv } from './env.ts';
+import { getEnv } from '@utils/env.ts';
 import chalk from 'chalk';
 import { appendFileSync } from 'fs';
 
+/**
+ * A helper class for logging messages with different log levels.
+ * Supports logging messages to the terminal and appending them to a log file
+ * based of the environment en log level config
+ *
+ * @class Logging
+ */
 export class Logging {
 	/**
-	 * Logs info messages to the terminal.
+	 * Logs an informational message to the terminal and the log file (if the log level is 'info' or 'all').
+	 * The message is prefixed with a timestamp and the log level 'INFO'.
 	 *
-	 * @param {string} message - The message that needs to be logged.
-	 * @returns void - Returns nothing.
+	 * @param {string|number} message - The message to be logged.
+	 * @returns {void} - This method does not return a value.
 	 */
-	static info(message: string): void {
+	static info(message: string|number): void {
 		const now = new Date();
 
 		console.log(`[${String(now.getDate()).padStart(2, '0')}-${String(now.getMonth() + 1).padStart(2, '0')}-${now.getFullYear()} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}] [${chalk.green('INFO')}]  ${message}`);
 
 		if (getEnv('LOG_LEVEL') == 'info' || getEnv('LOG_LEVEL') == 'all') {
 			appendFileSync(
-				'./logs/app.log',
+				`${<string>getEnv('MODULES_BASE_PATH')}logs/app.log`,
 				`[${String(now.getDate()).padStart(2, '0')}-${String(now.getMonth() + 1).padStart(2, '0')}-${now.getFullYear()} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}] [INFO]  ${message}\n`,
 				'utf-8'
 			);
@@ -24,19 +32,20 @@ export class Logging {
 	}
 
 	/**
-	 * Logs warning messages to the terminal.
+	 * Logs a warning message to the terminal and the log file (if the log level is 'warn' or 'all').
+	 * The message is prefixed with a timestamp and the log level 'WARN'.
 	 *
-	 * @param {string} message - The message that needs to be logged.
-	 * @returns void - Returns nothing.
+	 * @param {string|number} message - The warning message to be logged.
+	 * @returns {void} - This method does not return a value.
 	 */
-	static warn(message: string): void {
+	static warn(message: string|number): void {
 		const now = new Date();
 
 		console.log(`[${String(now.getDate()).padStart(2, '0')}-${String(now.getMonth() + 1).padStart(2, '0')}-${now.getFullYear()} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}] [${chalk.yellow('WARN')}]  ${message}`);
 
 		if (getEnv('LOG_LEVEL') == 'warn' || getEnv('LOG_LEVEL') == 'all') {
 			appendFileSync(
-				'./logs/app.log',
+				`${<string>getEnv('MODULES_BASE_PATH')}logs/app.log`,
 				`[${String(now.getDate()).padStart(2, '0')}-${String(now.getMonth() + 1).padStart(2, '0')}-${now.getFullYear()} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}] [WARN]  ${message}\n`,
 				'utf-8'
 			);
@@ -44,19 +53,20 @@ export class Logging {
 	}
 
 	/**
-	 * Logs error messages to the terminal.
+	 * Logs an error message to the terminal and the log file (if the log level is 'error' or 'all').
+	 * The message is prefixed with a timestamp and the log level 'ERROR'.
 	 *
-	 * @param {string} message - The message that needs to be logged.
-	 * @returns void - Returns nothing.
+	 * @param {string|number} message - The error message to be logged.
+	 * @returns {void} - This method does not return a value.
 	 */
-	static error(message: string): void {
+	static error(message: string|number): void {
 		const now = new Date();
 
 		console.log(`[${String(now.getDate()).padStart(2, '0')}-${String(now.getMonth() + 1).padStart(2, '0')}-${now.getFullYear()} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}] [${chalk.red('ERROR')}] ${message}`);
 
 		if (getEnv('LOG_LEVEL') == 'error' || getEnv('LOG_LEVEL') == 'all') {
 			appendFileSync(
-				'./logs/app.log',
+				`${<string>getEnv('MODULES_BASE_PATH')}logs/app.log`,
 				`[${String(now.getDate()).padStart(2, '0')}-${String(now.getMonth() + 1).padStart(2, '0')}-${now.getFullYear()} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}] [ERROR] ${message}\n`,
 				'utf-8'
 			);
@@ -64,25 +74,26 @@ export class Logging {
 	}
 
 	/**
-	 * Logs debug messages to the terminal.
-	 * It display the error message only if environment is set to 'debug'.
+	 * Logs a debug message to the terminal and the log file (if the environment is 'debug'
+	 * and the log level is 'debug' or 'all'). The message is prefixed with a timestamp and the log level 'DEBUG'.
+	 * Debug messages are only logged when the environment variable 'ENVIRONMENT' is set to 'debug'.
 	 *
-	 * @param {string} message - The message that needs to be logged.
-	 * @returns void - Returns nothing.
+	 * @param {string|number} message - The debug message to be logged.
+	 * @returns {void} - This method does not return a value.
 	 */
-	static debug(message: string): void {
-		if (getEnv('ENVIRONMENT') !== 'debug') return;
+	static debug(message: string|number): void {
 		const now = new Date();
+
+		if (getEnv('ENVIRONMENT') !== 'debug') return;
 
 		console.log(`[${String(now.getDate()).padStart(2, '0')}-${String(now.getMonth() + 1).padStart(2, '0')}-${now.getFullYear()} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}] [${chalk.blue('DEBUG')}] ${message}`);
 
 		if (getEnv('LOG_LEVEL') == 'debug' || getEnv('LOG_LEVEL') == 'all') {
 			appendFileSync(
-				'./logs/app.log',
+				`${<string>getEnv('MODULES_BASE_PATH')}logs/app.log`,
 				`[${String(now.getDate()).padStart(2, '0')}-${String(now.getMonth() + 1).padStart(2, '0')}-${now.getFullYear()} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getUTCSeconds()).padStart(2, '0')}] [DEBUG] ${message}\n`,
 				'utf-8'
 			);
 		}
 	}
-
 }
