23e28f0422b4b1a32374f389c07a72d6b79454e9
AeroBytesNL <kelvin@aerobytes.nl>
2025-06-03 12:49:09 +0200

    Fixed some logging errors. Moved enum to own file

--- enums/ColorEnum.ts
+++ enums/ColorEnum.ts
@@ -4,3 +4,10 @@ export enum ColorEnum {
 	Red 	= 0xFF0000,
 	AeroBytesBlue    = 0x069AF3
 }
+
+export enum Color {
+	Green   = 0x00FF00,
+	Orange  = 0xFFA500,
+	Red 	= 0xFF0000,
+	AeroBytesBlue    = 0x069AF3
+}
